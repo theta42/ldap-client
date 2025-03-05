f6162be2e139b59cf660417ff4f22d516106e834
William Mantly <wmantly@gmail.com>
2025-03-05 02:54:07 +0000

    Update files/ldap-ssh-key.sh

--- files/ldap-ssh-key.sh
+++ files/ldap-ssh-key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ldapsearch -H "ldap://10.1.0.55" \
-  -D "cn=ldapclient service,ou=People,dc=theta42,dc=com" \
-  -w "1lovebyte" \
-  -b "ou=People,dc=theta42,dc=com" \
-  "(&(uid=$1)(|(memberof=cn=host_access,ou=Groups,dc=theta42,dc=com)(memberof=cn=host_ldap-client-test_access,ou=Groups,dc=theta42,dc=com)))" \
+ldapsearch -H "{{ldap_host}}" \
+  -D "{{ldap_bind_dn}}" \
+  -w "{{ldap_bind_password}}" \
+  -b "ou=People,{{ldap_base_dn}}" \
+  "(&(uid=$1)(|(memberof=cn=host_access,ou=Groups,{{ldap_base_dn}})(memberof=cn=host_{{current_host}}_access,ou=Groups,{{ldap_base_dn}})))" \
   '*' | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
   
